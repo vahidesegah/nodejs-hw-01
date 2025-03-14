7d25784b05a9a32fef414cbb0da899255927ff1b
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-14 12:36:08 +0300

    addOne getAll

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,17 @@
-export const addOneContact = async () => {};
+import {createFakeContact } from "../utils/createFakeContact.js";
+import {readContacts} from "../utils/readContacts.js";
+import {writeContacts} from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    try {
+    const newContact = createFakeContact();
+    const contacts = await readContacts();
+    contacts.push(newContact);
+    await writeContacts(contacts);
+    console.log("One Contact Added!");
+    }
+    catch (error) {
+        console.error(error);    }
+};
 
 addOneContact();
