2d458b66ee28f2f6aa21e277ef352a98b7c11634
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-14 12:44:08 +0300

    remove all

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,3 +1,12 @@
-export const removeAllContacts = async () => {};
+import {writeContacts} from "../utils/writeContacts.js";
+
+export const removeAllContacts = async () => {
+    try {
+        await writeContacts([]);
+        console.log("All Contacts Deleted");
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 removeAllContacts();
