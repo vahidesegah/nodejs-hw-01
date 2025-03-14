94665406b43e0cd2f65a70d74f64b609fc984380
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-14 12:01:50 +0300

    readWrite

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,16 @@
+import fs from "fs/promises";
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB,"utf-8");
+        const jsonData = JSON.parse(data);
+        return jsonData;
+    }
+        catch (error) {
+            console.error(error);
+        }
+    };
+
+
+
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,14 @@
+import fs from "fs/promises";
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+        const dataToWrite = JSON.stringify(updatedContacts);
+        await fs.writeFile(PATH_DB, dataToWrite);
+        return dataToWrite;
+    }
+    catch (error) {
+        console.error(error);
+
+    }
+};
