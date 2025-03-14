12292ee0ae82859973f3701a10835312d6606781
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-14 12:27:01 +0300

    generate

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,22 @@
-const generateContacts = async (number) => {};
+import {createFakeContact } from "../utils/createFakeContact.js";
+import {readContacts} from "../utils/readContacts.js";
+import {writeContacts} from "../utils/writeContacts.js";
+
+
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+
+    for (let i=0; i<number; i++) {
+        contacts.push(createFakeContact());
+    }
+    try {
+        await writeContacts(contacts);
+        }
+        catch (error) {
+console.error(error);
+        }
+
+};
+
 
 generateContacts(5);
