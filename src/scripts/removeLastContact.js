d1e87bcc92d4082beab6f89d79abeb1e808f7f34
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-14 13:06:06 +0300

    remove last

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,20 @@
-export const removeLastContact = async () => {};
+import {readContacts} from "../utils/readContacts.js";
+import {writeContacts} from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    try {
+        const contacts = await readContacts();
+        if (contacts.length > 0) {
+        contacts.pop();
+        await writeContacts(contacts);
+        console.log("Tha Last One Removed");
+        } else {
+            console.log("No More Contacts Left");
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 removeLastContact();
